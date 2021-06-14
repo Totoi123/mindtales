8333913604ff0740ee8cb9837f344010c65bdf03
Naocha Luwang <naochaluwang@pop-os.localdomain>
2021-06-14 23:55:02 +0530

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   const { user, login, logout } = useContext(AuthContext);
   console.log(user);
   return (
-    <nav className=" container navbar p-4">
+    <nav className=" container navbar p-4 has-shadow ">
       <div className="navbar-header">
         <div className="navbar-brand">
           <h1 className="is-size-1 title has-text-darkgrey">Mindtales</h1>
@@ -26,20 +26,31 @@ const Navbar = () => {
             <a className="navbar-item is-size-4">Contact</a>
           </Link>
 
+          {user && (
+            <p className="navbar-item is-size-5 has-text-link">
+              Hello , {user.user_metadata.full_name}
+            </p>
+          )}
+
           <div className="navbar-item">
             <div className="buttons">
-              <p
-                onClick={login}
-                className="button is-link is-outlined is-rounded"
-              >
-                <strong>Login/Signup</strong>
-              </p>
-              <p
-                onClick={logout}
-                className="button is-danger is-outlined is-rounded"
-              >
-                <strong>Logout</strong>
-              </p>
+              {!user && (
+                <p
+                  onClick={login}
+                  className="button is-link is-outlined is-rounded"
+                >
+                  <strong>Login/Signup</strong>
+                </p>
+              )}
+
+              {user && (
+                <p
+                  onClick={logout}
+                  className="button is-danger is-outlined is-rounded"
+                >
+                  <strong>Logout</strong>
+                </p>
+              )}
             </div>
           </div>
         </div>
