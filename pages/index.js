40ad8a79132796c39d8535fb05d2a7a8a43d25fb
Naocha Luwang <naochaluwang@pop-os.localdomain>
2021-06-15 00:50:24 +0530

    added firebase

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { useContext } from "react";
 import AuthContext from "../stores/authContext";
+import { db } from "../firebase";
+import firebase from "firebase";
 
 const HomePage = () => {
   const { user, login } = useContext(AuthContext);
